ac6e3e9857b1f152606cd3f2825eda177fb9804d
wswind <18399096+wswind@users.noreply.github.com>
2020-05-13 10:34:10 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using Tesseract;
+
+namespace Learn_Tesseract_ocr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var img = Pix.LoadFromFile("tessdata\\01.png");    //需要识别的图片
+            var ocr = new TesseractEngine(@"./tessdata", "chi_sim", EngineMode.Default);    //使用chi_sim中文语言包做测试
+            var page = ocr.Process(img);
+            Console.Write(page.GetText());
+            Console.ReadKey();
+        }
+    }
+}
